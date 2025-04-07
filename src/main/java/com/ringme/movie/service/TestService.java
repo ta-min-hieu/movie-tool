ebf6f7990b8755ac4aaf5777ef3e950f2d9b8d66
hieutm <hieutm@ringme.vn>
2025-04-07 21:02:12 +0700

    update code

--- src/main/java/com/ringme/movie/service/TestService.java
+++ src/main/java/com/ringme/movie/service/TestService.java
@@ -1,44 +1,7 @@
 package com.ringme.movie.service;
 
-import com.ringme.movie.model.VcsMedia;
-import com.ringme.movie.model.VcsMediaSubtile;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.multipart.MultipartFile;
-
 public interface TestService {
     void callApiConvertFileInFolder(String folderPath);
 
     void standardFileName(String folderPath);
-
-    void movieExport(String folderPath, String filePath, String fileName);
-
-    void addMovieByFolder(String folderPath, Long episodeParent);
-
-    void generateSubtitleFromMkvPath(String folderPath);
-
-    void storeMedia(MultipartFile excelFile);
-
-    void getMoviePreview(HttpServletResponse response, String folderPath);
-
-    void getMovieShort(HttpServletResponse response, String folderPath, int movieQuality);
-
-    void addTextIntoFile(String filePath, String text);
-
-    void executeCommand(String command);
-
-    void generateSubtile();
-
-    void subtitleHandler(VcsMedia media);
-
-    void generateSubtileForCms(VcsMediaSubtile subtile, VcsMedia media);
-
-    void standardPlaylistFile();
-
-    void standardImageFile();
-
-    void standardMediaTime();
-
-    void generateSubtitleFromMkv();
-
-    void filterErrorSubtitle();
 }
